be97adb5edd8f4ebbcfe981e36bc8f9b5588c703
mauriciodelrio <mauricio.delrioz@alumnos.usm.cl>
2016-12-27 10:47:32 -0300

    fix comments

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,7 @@
-config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
