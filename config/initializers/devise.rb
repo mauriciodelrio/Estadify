8443cd4d7cad3150358636d1205400a474bd5856
mauriciodelrio <mauricio.delrioz@alumnos.usm.cl>
2016-12-27 10:45:01 -0300

    fix comments

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,4 @@
+config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
@@ -271,5 +272,4 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 end
